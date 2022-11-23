1c41e7b4b5668a9c5967cd1a032f434fd8d3ab59
ECON <lgyoon621@gmail.com>
2022-11-23 13:37:55 +0900

    [ADD] GlobalAuthFilter 필터 추가

--- src/main/java/com/example/gateway/GateWayApplication.java
+++ src/main/java/com/example/gateway/GateWayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GateWayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
     }
