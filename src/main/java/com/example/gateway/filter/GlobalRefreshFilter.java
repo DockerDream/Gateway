97ac3780978334e3d192034e9c0fbd5781d8ab7e
ECON <lgyoon621@gmail.com>
2022-11-25 15:36:09 +0900

    [ADD] 로그인 유지 관련 클래스 추가

--- src/main/java/com/example/gateway/filter/GlobalRefreshFilter.java
+++ src/main/java/com/example/gateway/filter/GlobalRefreshFilter.java
@@ -1,5 +1,6 @@
 package com.example.gateway.filter;
 
+
 import com.example.gateway.validation.JwtUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +11,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class GlobalAuthFilter extends AbstractGatewayFilterFactory {
-
+public class GlobalRefreshFilter extends AbstractGatewayFilterFactory {
     private final ObjectMapper objectMapper;
     private final JwtUtil jwtUtil;
 
@@ -27,12 +28,19 @@ public class GlobalAuthFilter extends AbstractGatewayFilterFactory {
         return !request.getHeaders().containsKey("token");
     }
 
+    private boolean isRefreshMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("refresh");
+    }
+
     @Override
     public GatewayFilter apply(Object config) {
         return (((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             if (this.isAuthMissing(request)) {
                 return jwtUtil.onError(exchange, "token header is missing in request", HttpStatus.UNAUTHORIZED);
+            }
+            if (this.isRefreshMissing(request)) {
+                return jwtUtil.onError(exchange, "refresh header is missing in request", HttpStatus.UNAUTHORIZED);
             }
             final String token = this.getAuthHeader(request);
             if (jwtUtil.isInvalid(token)) {
@@ -43,3 +51,4 @@ public class GlobalAuthFilter extends AbstractGatewayFilterFactory {
         }));
     }
 }
+
